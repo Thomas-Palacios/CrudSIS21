211d8247fd13a03c7c987734a1f00853ec932430
Thomas-Palacios <64821362+Thomas-Palacios@users.noreply.github.com>
2020-09-22 09:08:10 -0600

    CRUD SQllite 2020

---
+++ app/src/main/java/com/example/crud2020/detalles_articulos.java
@@ -0,0 +1,13 @@
+package com.example.crud2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class detalles_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_articulos);
+    }
+}
---
+++ app/src/main/java/com/example/crud2020/list_view_articulos.java
@@ -0,0 +1,13 @@
+package com.example.crud2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class list_view_articulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view_articulos);
+    }
+}
